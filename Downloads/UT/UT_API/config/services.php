e38187558472e6c1b6d68f0ff34bb7424a1de1a2
Matita <josegm23c@gmail.com>
2026-06-05 01:29:04 -0700

    Delete Downloads/UT/UT_API directory

--- Downloads/UT/UT_API/config/services.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'ses' => [
-        'key' => env('MAIL_USERNAME'),
-        'secret' => env('MAIL_PASSWORD'),
-        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
-    ],
-];
